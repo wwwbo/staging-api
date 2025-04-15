89d305542cb9598d448b9a415546d80827c93956
Wowo Wibowo <wowow.908@gmail.com>
2025-04-15 23:30:10 +0700

    grouping api prefix

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,17 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\AuthController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/auth/register', [AuthController::class, 'register']);
-Route::post('/auth/login', [AuthController::class, 'login']);
+Route::prefix('v1')->group(function () {
+    Route::prefix('auth')->group(function () {
+        Route::post('/register', [AuthController::class, 'register']);
+        Route::post('/login', [AuthController::class, 'login']);
+    });
+});
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+// Next enchancement
+Route::prefix('v2')->group(function () {
+    // path api
+});
