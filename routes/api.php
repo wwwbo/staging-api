c330b5c6c7e5a6983c95db1eb615c5f9b4a0f5e5
Wowo Wibowo <wowow.908@gmail.com>
2025-04-15 18:54:45 +0700

    add new path for register

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/auth/register', [AuthController::class, 'register']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
