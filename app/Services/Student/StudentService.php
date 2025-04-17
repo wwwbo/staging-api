477fd90e27087dbb0a0f54d7cb5326917652fc6a
Wowo Wibowo <wowow.908@gmail.com>
2025-04-17 09:35:07 +0700

    [FEATURE] - implement exception

--- app/Services/Student/StudentService.php
+++ app/Services/Student/StudentService.php
@@ -3,6 +3,7 @@
 namespace App\Services\Student;
 
 use App\Models\Student;
+use Exception;
 use Illuminate\Support\Facades\Storage;
 
 class StudentService
@@ -14,30 +15,42 @@ class StudentService
 
     public function store($data)
     {
-        if ($data->hasFile('photo')) {
-            $image = $data->file('photo');
-            $image->storeAs('student', $image->hashName());
-            $userPhoto = $image->hashName();
-        } else {
-            $userPhoto = 'images/default.jpg';
+        try {
+            if ($data->hasFile('photo')) {
+                $image = $data->file('photo');
+                $image->storeAs('student', $image->hashName());
+                $userPhoto = $image->hashName();
+            } else {
+                $userPhoto = 'images/default.jpg';
+            }
+
+            $checkEmailExist = Student::where('email', $data['email'])->exists();
+
+            if ($checkEmailExist)
+                throw new Exception('Data already exist', 409);
+
+            $student = Student::create([
+                'name' => $data->name,
+                'email' => $data->email,
+                'religion' => $data->religion,
+                'place_of_birth' => $data->place_of_birth,
+                'date_of_birth' => $data->date_of_birth,
+                'gender' => $data->gender,
+                'address' => $data->address,
+                'phone_number' => $data->phone_number,
+                'photo' => $userPhoto
+            ]);
+
+            return [
+                'status' => $student ? 'success' : 'error',
+                'message' => $student ? 'Success create data' : 'Failed create data'
+            ];
+        } catch (Exception $e) {
+            return [
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ];
         }
-
-        $student = Student::create([
-            'name' => $data->name,
-            'email' => $data->email,
-            'religion' => $data->religion,
-            'place_of_birth' => $data->place_of_birth,
-            'date_of_birth' => $data->date_of_birth,
-            'gender' => $data->gender,
-            'address' => $data->address,
-            'phone_number' => $data->phone_number,
-            'photo' => $userPhoto
-        ]);
-
-        return [
-            'status' => $student ? 'success' : 'error',
-            'message' => $student ? 'Success create data' : 'Failed create data'
-        ];
     }
 
     public function show(string $userId)
@@ -47,53 +60,67 @@ class StudentService
 
     public function update($data, $userId)
     {
-        $student = Student::findOrFail($userId);
+        try {
+            $student = Student::findOrFail($userId);
 
-        if ($data->hasFile('photo')) {
-            Storage::delete('student/' . $student->image);
+            if ($data->hasFile('photo')) {
+                Storage::delete('student/' . $student->image);
 
-            $image = $data->file('photo');
-            $image->storeAs('student', $image->hashName());
+                $image = $data->file('photo');
+                $image->storeAs('student', $image->hashName());
 
-            $student->update([
-                'name' => $data->name,
-                'email' => $data->email,
-                'religion' => $data->religion,
-                'place_of_birth' => $data->place_of_birth,
-                'date_of_birth' => $data->date_of_birth,
-                'gender' => $data->gender,
-                'address' => $data->address,
-                'phone_number' => $data->phone_number,
-                'photo' => $image->hashName()
-            ]);
-        } else {
-            $student->update([
-                'name' => $data->name,
-                'email' => $data->email,
-                'religion' => $data->religion,
-                'place_of_birth' => $data->place_of_birth,
-                'date_of_birth' => $data->date_of_birth,
-                'gender' => $data->gender,
-                'address' => $data->address,
-                'phone_number' => $data->phone_number,
-            ]);
+                $student->update([
+                    'name' => $data->name,
+                    'email' => $data->email,
+                    'religion' => $data->religion,
+                    'place_of_birth' => $data->place_of_birth,
+                    'date_of_birth' => $data->date_of_birth,
+                    'gender' => $data->gender,
+                    'address' => $data->address,
+                    'phone_number' => $data->phone_number,
+                    'photo' => $image->hashName()
+                ]);
+            } else {
+                $student->update([
+                    'name' => $data->name,
+                    'email' => $data->email,
+                    'religion' => $data->religion,
+                    'place_of_birth' => $data->place_of_birth,
+                    'date_of_birth' => $data->date_of_birth,
+                    'gender' => $data->gender,
+                    'address' => $data->address,
+                    'phone_number' => $data->phone_number,
+                ]);
+            }
+
+            return [
+                'status' => $student ? 'success' : 'error',
+                'message' => $student ? 'Success update data' : 'Failed update data'
+            ];
+        } catch (Exception $e) {
+            return [
+                'status' => 'error',
+                'message' => 'Failed update data'
+            ];
         }
-
-        return [
-            'status' => $student ? 'success' : 'error',
-            'message' => $student ? 'Success update data' : 'Failed update data'
-        ];
     }
 
     public function destroy($userId)
     {
-        $student = Student::findOrFail($userId);
-        Storage::delete('student/' . $student->photo);
-        Student::find($student['id'])->delete();
+        try {
+            $student = Student::findOrFail($userId);
+            Storage::delete('student/' . $student->photo);
+            Student::find($student['id'])->delete();
 
-        return [
-            'status' => $student ? 'success' : 'error',
-            'message' => $student ? 'Success delete data' : 'Failed delete data'
-        ];
+            return [
+                'status' => $student ? 'success' : 'error',
+                'message' => $student ? 'Success delete data' : 'Failed delete data'
+            ];
+        } catch (Exception $e) {
+            return [
+                'status' => 'error',
+                'message' => 'Failed to delete data'
+            ];
+        }
     }
 }
